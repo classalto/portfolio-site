5605e5ddcf9e56faab2f3a9d795893bee129473a
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-07-21 17:38:06 -0700

    style social icon / header / navlinks

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,30 +1,33 @@
 /* eslint-disable max-len */
 import React from 'react';
 import styles from './home.module.css';
-import linkedin from '../assets/linkedin.svg';
-import github from '../assets/github.svg';
+import { GrLinkedin, GrGithub } from 'react-icons/gr';
 
 const Home = () => {
   return (
     <div className={styles.home}>
-      <aside className={styles.box}>
+      <figure className={styles.box}>
         <div className={styles.wave}></div>
-      </aside>
+      </figure>
+      <header>
+        <nav className={styles.navigation}>
+          <ul className={styles.navlinks}>
+            <li>Resume</li>
+            <li>Projects</li>
+          </ul>
+        </nav>
+        <section className={styles.social}>
+          <GrLinkedin className={styles.GrLinkedin} />
+          <GrGithub className={styles.GrGithub} />
+        </section>      
+      </header> 
       <section className={styles.heading}>
         <h1>Victor Williams</h1>
         <span>Software Engineer</span>
         <p>Collaborative Engineer solving real-world problems with elegant and efficient solutions. <br/>
         After years of collaborating with peers from a diverse range of backgrounds, <br/>
         I aim to implement seamless and easy to use designs that are team-built and enjoyable to use!</p>
-      </section>      
-      <nav className={styles.navlinks}>
-        <span>Resume</span>
-        <span>Projects</span>
-      </nav>
-      <section className={styles.social}>
-        <img src={linkedin} alt="linkedin" />
-        <img src={github} alt="github" />
-      </section>
+      </section>     
     </div>
   );
 };
